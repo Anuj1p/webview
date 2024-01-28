b17fe4d58cb62820691cb0e9a91cd1c62fb2fe3d
Anuj | <anuj1@FIN-CIT-346.local>
2024-01-28 17:05:04 +0530

    Add dashboard route

---
+++ pages/dashboard.tsx
@@ -0,0 +1,12 @@
+import Link from "next/link";
+
+const Dashboard = () => {
+  return (
+    <div>
+      <h1>Dashboard</h1>
+      <Link href="/webview">Go to webview</Link>
+    </div>
+  );
+}
+
+export default Dashboard
